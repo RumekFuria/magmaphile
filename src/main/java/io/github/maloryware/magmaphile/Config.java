275d21d4c20339c024f5f6503372edb84239f152
maloryware <mal.larsson.reyes@gmail.com>
2025-02-20 21:43:22 +0000

    Added lower bound configurability

--- src/main/java/io/github/maloryware/magmaphile/Config.java
+++ src/main/java/io/github/maloryware/magmaphile/Config.java
@@ -5,6 +5,13 @@ import eu.midnightdust.lib.config.MidnightConfig;
 public class Config extends MidnightConfig {
 
     public static final String GENERAL = "general";
-    @Entry(category = GENERAL) public static int lavaHeightGen = -117;
+
+    @Entry(category = GENERAL) public static boolean override_lava_gen;
+    @Entry(category = GENERAL) public static int lava_max_height_gen = 0;
+    @Entry(category = GENERAL) public static boolean toggle_lower_bound;
+    @Entry(category = GENERAL) public static int lava_min_height_gen = 0;
+
+
+
 
 }
--- src/main/java/io/github/maloryware/magmaphile/mixin/LavaWorldGenHeight.java
+++ src/main/java/io/github/maloryware/magmaphile/mixin/LavaWorldGenHeight.java
@@ -1,6 +1,10 @@
 package io.github.maloryware.magmaphile.mixin;
 
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.llamalad7.mixinextras.sugar.Local;
 import io.github.maloryware.magmaphile.Config;
 import net.minecraft.block.Blocks;
 import net.minecraft.world.gen.chunk.AquiferSampler;
@@ -14,17 +18,33 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(NoiseChunkGenerator.class)
 public class LavaWorldGenHeight {
 
-	// yes re-hardcoding is not the best decision but shrug
+
 	@ModifyVariable(method = "createFluidLevelSampler",	at = @At(value = "INVOKE", target = "Lnet/minecraft/world/gen/chunk/AquiferSampler$FluidLevel;<init>(ILnet/minecraft/block/BlockState;)V"), index = 1)
 	private static AquiferSampler.FluidLevel lavaHeightLevel(AquiferSampler.FluidLevel value){
-		return new AquiferSampler.FluidLevel(Config.lavaHeightGen, Blocks.LAVA.getDefaultState());
+		return new AquiferSampler.FluidLevel(Config.lava_max_height_gen, Blocks.LAVA.getDefaultState());
 	}
 
-	// ditto
 	// catches synthetic method
 	@ModifyExpressionValue(method = "method_45509", at = @At(value = "CONSTANT", args = "intValue=-54"))
-	private static int lava(int original){
-		return Config.lavaHeightGen;
+	private static int lavaGenHigherBound(int original){
+		return Config.override_lava_gen ? Config.lava_max_height_gen : original;
 	}
 
+	// VERY sloppy, but couldn't get expressions going properly, so this'll have to do
+
+	@SuppressWarnings("LocalMayBeArgsOnly")
+    @ModifyReturnValue(method = "method_45509", at = @At("RETURN"))
+	private static AquiferSampler.FluidLevel lavaGenLowerBound(AquiferSampler.FluidLevel original,
+															   @Local(ordinal = 2) int y,
+															   @Local(ordinal = 0) AquiferSampler.FluidLevel fluidLevel,
+															   @Local(ordinal = 1) AquiferSampler.FluidLevel fluidLevel2){
+
+		if (!Config.override_lava_gen) { return original; }
+		boolean check = Config.toggle_lower_bound
+				? y < Config.lava_max_height_gen
+				: y < Config.lava_max_height_gen && y > Config.lava_min_height_gen;
+
+        return check ? fluidLevel : fluidLevel2;
+
+	}
 }
